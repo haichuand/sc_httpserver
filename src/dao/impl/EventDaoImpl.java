411c555b39db2933473c4486957944954d836292
Xuejing Dong <fzudxj@gmail.com>
2016-04-08 21:49:17 -0700

    add event and user queries.

--- src/dao/impl/EventDaoImpl.java
+++ src/dao/impl/EventDaoImpl.java
@@ -74,32 +74,98 @@ private SessionFactory factory;
 
 	@Override
 	public void edit(Event event) {
-		// TODO Auto-generated method stub
+		Session session = factory.openSession();
+    Transaction tx = null;
+    try{
+      tx = session.beginTransaction();
+      String hql = "UPDATE event set" + 
+            "even_type = : ev_type" + 
+            "title = : ev_title" + 
+            "location = : ev_location" + 
+            "start_time = : ev_start" + 
+            "end_time = : ev_end" + 
+            "creator_id = : ev_creator" + 
+            "create_time = : ev_ctime" + 
+            "WHERE event_id = : ev_id";
 
+      Query query = session.createQuery(hql);
+      query.setParameter("ev_id", event.getEventId());
+      query.setParameter("ev_type", event.getEventType());
+      query.setParameter("ev_title", event.getEventType());
+      query.setParameter("ev_location", event.getTitle());
+      query.setParameter("ev_start", event.getStartTime());
+      query.setParameter("ev_end", event.getEndTime());
+      query.setParameter("ev_creator", event.getCreatorId());
+      query.setParameter("ev_ctime", event.getCreateTime());
+
+      int result = query.executeUpdate();
+      System.out.println("Update finished. Rows affected: " + result);
+
+      tx.commit();
+    } catch (HibernateException e) {
+      System.out.println("Error");
+        if (tx!=null) tx.rollback();
+          e.printStackTrace(); 
+    } finally {
+        session.close(); 
+    }
 	}
 
 	@Override
 	public void delete(String eventId) {
-		// TODO Auto-generated method stub
+		Session session = factory.openSession();
+    Transaction tx = null;
+    try{
+      tx = session.beginTransaction();
+      String hql = "DELETE FROM event " + 
+            "WHERE event_id = : ev_id";
 
+      Query query = session.createQuery(hql);
+      query.setParameter("ev_id", eventId);
+
+      int result = query.executeUpdate();
+      System.out.println("Delete finished. Rows affected: " + result);
+
+      tx.commit();
+    } catch (HibernateException e) {
+      System.out.println("Error");
+        if (tx!=null) tx.rollback();
+          e.printStackTrace(); 
+    } finally {
+        session.close(); 
+    }
 	}
 
 	@Override
 	public Event getEvent(String eventId) {
-		// TODO Auto-generated method stub
-		return null;
+		Session session = factory.openSession();
+    List result = null;
+    Transaction tx = null;
+    try{
+      tx = session.beginTransaction();
+      String hql = "FROM event E " + 
+            "WHERE E.event_id = : ev_id";
+
+      Query query = session.createQuery(hql);
+      query.setParameter("ev_id", eventId);
+
+      result = query.list();
+      System.out.println("GetEvent for " + eventId);
+
+      tx.commit();
+    } catch (HibernateException e) {
+      System.out.println("Error");
+        if (tx!=null) tx.rollback();
+          e.printStackTrace(); 
+    } finally {
+        session.close(); 
+    }
+		return result.get(0);
 	}
 
 	@Override
 	public Set getEventAttendees(String eventId) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Set getUserEvents(String userId) {
-		// TODO Auto-generated method stub
-		return null;
+    getEvent (eventId).getEventAttendees();
 	}
 
 }
